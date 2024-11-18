393087050edd86f0e86e3a1a6080737847e7b9c1
Sveta Miasota <pophopka@gmail.com>
2024-11-19 02:09:44 +0300

    data.sql file added

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,14 @@
+-- Database: secret_demo
+
+-- DROP DATABASE IF EXISTS secret_demo;
+
+CREATE DATABASE secret_demo
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1251'
+    LC_CTYPE = 'English_United States.1251'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
