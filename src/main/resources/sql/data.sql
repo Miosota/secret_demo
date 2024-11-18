eb0f9ba86705c58edec71306619bb73f88dd1c11
Sveta Miasota <pophopka@gmail.com>
2024-11-19 02:14:51 +0300

    fixed data.sql

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,14 +1,38 @@
--- Database: secret_demo
+-- Table: public.users
 
--- DROP DATABASE IF EXISTS secret_demo;
+-- DROP TABLE IF EXISTS public.users;
 
-CREATE DATABASE secret_demo
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United States.1251'
-    LC_CTYPE = 'English_United States.1251'
-    LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+CREATE TABLE IF NOT EXISTS public.users
+(
+    user_id integer NOT NULL DEFAULT nextval('users_user_id_seq'::regclass),
+    name character varying(100) COLLATE pg_catalog."default",
+    CONSTRAINT users_pkey PRIMARY KEY (user_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to postgres;
+    
+-- Table: public.secrets
+
+-- DROP TABLE IF EXISTS public.secrets;
+
+CREATE TABLE IF NOT EXISTS public.secrets
+(
+    secret_id integer NOT NULL DEFAULT nextval('secrets_secret_id_seq'::regclass),
+    owner_id integer,
+    secret_login character varying(60) COLLATE pg_catalog."default",
+    secret_password character varying(60) COLLATE pg_catalog."default",
+    times_to_view integer,
+    CONSTRAINT secrets_pkey PRIMARY KEY (secret_id),
+    CONSTRAINT secrets_owner_id_fkey FOREIGN KEY (owner_id)
+        REFERENCES public.users (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.secrets
+    OWNER to postgres;
