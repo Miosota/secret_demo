3c52f8ee0dfafc07d07bcfcbd42812be6187030d
Sveta Miasota <pophopka@gmail.com>
2024-11-25 18:12:26 +0300

    generate link for secret

--- spring-boot-server/src/main/java/org/secretproject/controller/CommonController.java
+++ spring-boot-server/src/main/java/org/secretproject/controller/CommonController.java
@@ -1,5 +1,6 @@
 package org.secretproject.controller;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +11,9 @@ import org.secretproject.service.SecretService;
 import org.secretproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,6 +23,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
@@ -30,6 +37,7 @@ import jakarta.persistence.PersistenceContext;
 @CrossOrigin
 public class CommonController {
 
+
     @Autowired
     UserService userService;
     @Autowired
@@ -40,7 +48,7 @@ public class CommonController {
 
     @PostMapping("/createobjects")
     @Transactional
-    public void createUser(@RequestBody CommonObject commonObject){
+    public ResponseEntity<String> createUser(@RequestBody CommonObject commonObject){
         User user = new User(commonObject.getUserName());
         Secret secret = new Secret(user, commonObject.getSecretText(), commonObject.getTimesToView());
 
@@ -54,6 +62,13 @@ public class CommonController {
         userService.createUser(user);
         System.out.println("Test : userId:"+user.getId());
         secretService.createSecret(secret);
+        UriComponents builder = UriComponentsBuilder.newInstance()
+        .scheme("http")
+        .host("localhost:3000")
+        .path("/secrets")
+        .queryParam("id", secret.getId()).build();
+
+    return new ResponseEntity<String>("Test : userId:"+user.getId()+ " ; secretData: id: "+secret.getId() + " : share secret with your friends : "+builder.toString(), HttpStatus.OK);
     }
 
 }
